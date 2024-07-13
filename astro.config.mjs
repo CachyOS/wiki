692d083c19f4fd74b637d37d020eba967f6d2d98
SoulHarsh007 <harsh.peshwani@outlook.com>
2024-07-14 03:30:40 +0530

    feat: installation on VirtualBox and VMware (#35)
    Add guides for installing CachyOS on VirtualBox and VMware
    
    bump deps and add missing `prettier-plugin-astro` dev dependency
    
    cleanup `image-component.tsx` and include optional `alt` attribute
    (defaults to a blank string)
    
    Signed-off-by: SoulHarsh007 <harsh.peshwani@outlook.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -36,7 +36,7 @@ export default defineConfig({
       lastUpdated: true,
       customCss: ['./src/tailwind.css'],
       components: {
-        Head: "./src/components/starlight/Head.astro",
+        Head: './src/components/starlight/Head.astro',
       },
       title: 'CachyOS',
       logo: {
@@ -55,9 +55,7 @@ export default defineConfig({
       },
       expressiveCode: {
         shiki: {
-          langs: [
-            'ini'
-          ],
+          langs: ['ini'],
         },
       },
       head: [
@@ -81,23 +79,23 @@ export default defineConfig({
         {
           label: 'Getting Started',
           translations: {
-            'sk': 'Začíname',
-            'cs': 'Začínáme',
+            sk: 'Začíname',
+            cs: 'Začínáme',
           },
           items: [
             {
               label: 'Why CachyOS?',
               translations: {
-                'sk': 'Prečo CachyOS?',
-                'cs': 'Proč CachyOS?',
+                sk: 'Prečo CachyOS?',
+                cs: 'Proč CachyOS?',
               },
               link: 'cachyos_basic/why_cachyos',
             },
             {
               label: 'Downloading CachyOS',
               translations: {
-                'sk': 'Sťahovanie CachyOS',
-                'cs': 'Stažení CachyOS',
+                sk: 'Sťahovanie CachyOS',
+                cs: 'Stažení CachyOS',
               },
               link: 'cachyos_basic/download',
             },
@@ -106,95 +104,111 @@ export default defineConfig({
         {
           label: 'Installation',
           translations: {
-            'sk': 'Inštalácia',
-            'cs': 'Instalace',
+            sk: 'Inštalácia',
+            cs: 'Instalace',
           },
           items: [
             {
               label: 'Installation Prepare',
               translations: {
-                'sk': 'Príprava na inštaláciu',
-                'cs': 'Příprava Instalace',
+                sk: 'Príprava na inštaláciu',
+                cs: 'Příprava Instalace',
               },
               link: 'installation/installation_prepare',
             },
             {
               label: 'Boot managers',
               translations: {
-                'sk': 'Boot manažéry',
-                'cs': 'Boot manažeři',
+                sk: 'Boot manažéry',
+                cs: 'Boot manažeři',
               },
               link: 'installation/bootmanagers',
             },
             {
               label: 'Filesystem',
               translations: {
-                'sk': 'Súborový systém',
-                'cs': 'Filesystem',
+                sk: 'Súborový systém',
+                cs: 'Filesystem',
               },
               link: 'installation/filesystem',
             },
             {
               label: 'Screenshots',
               translations: {
-                'sk': 'Snímky obrazovky',
-                'cs': 'Screenshots',
+                sk: 'Snímky obrazovky',
+                cs: 'Screenshots',
               },
               link: 'installation/screenshots',
             },
             {
               label: 'Desktop Environments',
               translations: {
-                'sk': 'Desktopové prostredia',
-                'cs': 'Desktop prostředí',
+                sk: 'Desktopové prostredia',
+                cs: 'Desktop prostředí',
               },
               link: 'installation/desktop_environments',
             },
             {
               label: 'Installation on Root',
               translations: {
-                'sk': 'Inštalácia Root',
-                'cs': 'Instalace na kořenový oddíl',
+                sk: 'Inštalácia Root',
+                cs: 'Instalace na kořenový oddíl',
               },
               link: 'installation/installation_on_root',
             },
             {
+              label: 'Installation on VirtualBox',
+              translations: {
+                sk: 'Inštalácia na VirtualBox',
+                cs: 'Instalace na VirtualBox',
+              },
+              link: 'installation/installation_virtualbox',
+            },
+            {
+              label: 'Installation on VMware Workstation',
+              translations: {
+                sk: 'Inštalácia na VMware Workstation',
+                cs: 'Instalace na VMware Workstation',
+              },
+              link: 'installation/installation_vmware',
+            },
+            {
               label: 'Installation Dual Boot',
               translations: {
-                'sk': 'Inštalácia Dual Root',
-                'cs': 'Instalace duálního zavaděče',
+                sk: 'Inštalácia Dual Root',
+                cs: 'Instalace duálního zavaděče',
               },
               link: 'installation/installation_dualboot',
             },
             {
               label: 'Installation T2 MacBook',
               translations: {
-                'sk': 'Inštalácia na T2 MacBook',
-                'cs': 'Instalace na MacBook T2',
+                sk: 'Inštalácia na T2 MacBook',
+                cs: 'Instalace na MacBook T2',
               },
               link: 'installation/installation_t2macbook',
             },
             {
               label: 'Installation on Root with Secure Boot',
               translations: {
-                'sk': 'Inštalácia Root so zabezpečeným spustením',
-                'cs': 'Instalace na kořenový oddíl s Secure Boot',
+                sk: 'Inštalácia Root so zabezpečeným spustením',
+                cs: 'Instalace na kořenový oddíl s Secure Boot',
               },
               link: 'installation/installation_secureboot',
             },
             {
               label: 'Installation Handheld Edition',
               translations: {
-                'sk': 'Inštalácia Handheld Edition',
-                'cs': 'Instalace verze pro přenosné zařízení',
+                sk: 'Inštalácia Handheld Edition',
+                cs: 'Instalace verze pro přenosné zařízení',
               },
               link: 'installation/installation_handheld',
             },
             {
               label: 'Updating CachyOS',
               translations: {
-                'sk': 'Aktualizácia CachyOS',
-                'cs': 'Aktualizace CachyOS',
+                sk: 'Aktualizácia CachyOS',
+                cs: 'Aktualizace CachyOS',
               },
               link: 'installation/updating_cachyos',
             },
@@ -203,23 +217,23 @@ export default defineConfig({
         {
           label: 'Configuration',
           translations: {
-            'sk': 'Konfigurácia',
-            'cs': 'Nastavení',
+            sk: 'Konfigurácia',
+            cs: 'Nastavení',
           },
           items: [
             {
               label: 'Post Install Setup',
               translations: {
-                'sk': 'Nastavenie po inštalácii',
-                'cs': 'Nastavení po instalaci',
+                sk: 'Nastavenie po inštalácii',
+                cs: 'Nastavení po instalaci',
               },
               link: 'configuration/post_install_setup',
             },
             {
               label: 'Dual GPU Laptops (NVIDIA + iGPU)',
               translations: {
-                'sk': 'Dvojité GPU notebooky (NVIDIA + iGPU)',
-                'cs': 'Notebooky s dvojitou GPU (NVIDIA + iGPU)',
+                sk: 'Dvojité GPU notebooky (NVIDIA + iGPU)',
+                cs: 'Notebooky s dvojitou GPU (NVIDIA + iGPU)',
               },
               link: 'configuration/dual_gpu',
             },
@@ -230,16 +244,16 @@ export default defineConfig({
             {
               label: 'General System Tweaks',
               translations: {
-                'sk': 'Všeobecné systémové úpravy',
-                'cs': 'Obecná úprava systému',
+                sk: 'Všeobecné systémové úpravy',
+                cs: 'Obecná úprava systému',
               },
               link: 'configuration/general_system_tweaks',
             },
             {
               label: 'Kernel Manager',
               translations: {
-                'sk': 'Správca jadra',
-                'cs': 'Správce jader',
+                sk: 'Správca jadra',
+                cs: 'Správce jader',
               },
               link: 'configuration/kernel-manager',
             },
@@ -272,31 +286,31 @@ export default defineConfig({
         {
           label: 'CachyOS Repositories',
           translations: {
-            'sk': 'CachyOS Repozitáre',
-            'cs': 'CachyOS repozitáře',
+            sk: 'CachyOS Repozitáre',
+            cs: 'CachyOS repozitáře',
           },
           items: [
             {
               label: 'What are the CachyOS repositories?',
               translations: {
-                'sk': 'Čo sú repozitáre CachyOS?',
-                'cs': 'Co jsou CachyOS repozitáře?',
+                sk: 'Čo sú repozitáre CachyOS?',
+                cs: 'Co jsou CachyOS repozitáře?',
               },
               link: 'cachyos_repositories/what_are_the_cachyos_repo',
             },
             {
               label: 'How to add CachyOS repositories?',
               translations: {
-                'sk': 'Ako pridať repozitáre CachyOS?',
-                'cs': 'Jak přidat CachyOS repozitáře?',
+                sk: 'Ako pridať repozitáre CachyOS?',
+                cs: 'Jak přidat CachyOS repozitáře?',
               },
               link: 'cachyos_repositories/how_to_add_cachyos_repo',
             },
             {
               label: 'PGO, BOLT and other Optimization',
               translations: {
-                'sk': 'PGO, BOLT a iné optimalizácie',
-                'cs': 'PGO, BOLT a další optimalizace',
+                sk: 'PGO, BOLT a iné optimalizácie',
+                cs: 'PGO, BOLT a další optimalizace',
               },
               link: 'cachyos_repositories/other_optimization',
             },
@@ -305,31 +319,31 @@ export default defineConfig({
         {
           label: 'Kernel',
           translations: {
-            'sk': 'Jadro',
-            'cs': 'Kernel',
+            sk: 'Jadro',
+            cs: 'Kernel',
           },
           items: [
             {
               label: 'CachyOS Kernel Features',
               translations: {
-                'sk': 'Funkcie jadra CachyOS',
-                'cs': 'Funkce CachyOS kernelu',
+                sk: 'Funkcie jadra CachyOS',
+                cs: 'Funkce CachyOS kernelu',
               },
               link: 'kernel/kernel',
             },
             {
               label: 'sched-ext Tutorial',
               translations: {
-                'sk': 'sched-ext návod',
-                'cs': 'sched-ext Tutoriál',
+                sk: 'sched-ext návod',
+                cs: 'sched-ext Tutoriál',
               },
               link: 'kernel/sched-ext',
             },
             {
               label: 'Modules Handling (NVIDIA/ZFS)',
               translations: {
-                'sk': 'Správa modulov (NVIDIA/ZFS)',
-                'cs': 'Správa modulů (NVIDIA/ZFS)',
+                sk: 'Správa modulov (NVIDIA/ZFS)',
+                cs: 'Správa modulů (NVIDIA/ZFS)',
               },
               link: 'kernel/modules',
             },
@@ -338,46 +352,46 @@ export default defineConfig({
         {
           label: 'Support',
           translations: {
-            'sk': 'Podpora',
-            'cs': 'Podpora',
+            sk: 'Podpora',
+            cs: 'Podpora',
           },
           items: [
             {
               label: 'FAQ',
               translations: {
-                'cs': 'Často kladené otázky',
+                cs: 'Často kladené otázky',
               },
               link: 'support/faq',
             },
             {
               label: 'Troubleshooting',
               translations: {
-                'sk': 'Riešenie problémov',
-                'cs': 'Řešení potíží',
+                sk: 'Riešenie problémov',
+                cs: 'Řešení potíží',
               },
               link: 'support/troubleshooting',
             },
             {
               label: 'Getting Help',
               translations: {
-                'sk': 'Získanie pomoci',
-                'cs': 'Jak získat pomoc',
+                sk: 'Získanie pomoci',
+                cs: 'Jak získat pomoc',
               },
               link: 'support/getting_help',
             },
             {
               label: 'Submitting Bugs',
               translations: {
-                'sk': 'Odoslanie chýb',
-                'cs': 'Nahlášení chyb',
+                sk: 'Odoslanie chýb',
+                cs: 'Nahlášení chyb',
               },
               link: 'support/submitting_bugs',
             },
             {
               label: 'Submitting Package Requests',
               translations: {
-                'sk': 'Odoslanie požiadaviek na balíčky',
-                'cs': 'Žádosti o balíčky',
+                sk: 'Odoslanie požiadaviek na balíčky',
+                cs: 'Žádosti o balíčky',
               },
               link: 'support/submitting_package_requests',
             },
@@ -386,34 +400,33 @@ export default defineConfig({
         {
           label: 'Changelogs',
           translations: {
-            'sk': 'Zmenové logy',
-            'cs': 'Změny',
+            sk: 'Zmenové logy',
+            cs: 'Změny',
           },
           items: [
             {
               label: 'GUI Installer and ISO',
               translations: {
-                'sk': 'GUI Inštalátor a ISO',
-                'cs': 'Grafický instalátor a ISO',
+                sk: 'GUI Inštalátor a ISO',
+                cs: 'Grafický instalátor a ISO',
               },
               link: 'changelogs/gui_installer',
             },
             {
               label: 'CLI Installer',
               translations: {
-                'sk': 'CLI Inštalátor',
-                'cs': 'Příkazový řádek instalátoru',
+                sk: 'CLI Inštalátor',
+                cs: 'Příkazový řádek instalátoru',
               },
               link: 'changelogs/cli_installer',
             },
-
           ],
         },
         {
           label: 'Policy',
           translations: {
-            'sk': 'Politika',
-            'cs': 'Podmínky',
+            sk: 'Politika',
+            cs: 'Podmínky',
           },
           items: [
             {
@@ -423,48 +436,48 @@ export default defineConfig({
             {
               label: 'Code of Conduct',
               translations: {
-                'sk': 'Kódex správania',
-                'cs': 'Kodex chování',
+                sk: 'Kódex správania',
+                cs: 'Kodex chování',
               },
               link: 'policy/code_of_conduct',
             },
             {
               label: 'Terms of Service',
               translations: {
-                'sk': 'Podmienky služby',
-                'cs': 'Obchodní podmínky',
+                sk: 'Podmienky služby',
+                cs: 'Obchodní podmínky',
               },
               link: 'policy/terms_of_service',
             },
             {
               label: 'Privacy Policy',
               translations: {
-                'sk': 'Zásady ochrany osobných údajov',
-                'cs': 'Zásady ochrany osobních údajů',
+                sk: 'Zásady ochrany osobných údajov',
+                cs: 'Zásady ochrany osobních údajů',
               },
               link: 'policy/privacy_policy',
             },
             {
               label: 'Repository Policy',
               translations: {
-                'sk': 'Politika repozitárov',
-                'cs': 'Repozitářní politika',
+                sk: 'Politika repozitárov',
+                cs: 'Repozitářní politika',
               },
               link: 'policy/repository_policy',
             },
             {
               label: 'Social',
               translations: {
-                'sk': 'Sociálne siete',
-                'cs': 'Sociální sítě',
+                sk: 'Sociálne siete',
+                cs: 'Sociální sítě',
               },
               link: 'policy/social',
             },
             {
               label: 'Donation',
               translations: {
-                'sk': 'Darcovstvo',
-                'cs': 'Dobrovolné příspěvky',
+                sk: 'Darcovstvo',
+                cs: 'Dobrovolné příspěvky',
               },
               link: 'policy/donation',
             },
--- src/components/image-component.tsx
+++ src/components/image-component.tsx
@@ -1,10 +1,13 @@
 import { PhotoProvider, PhotoView } from 'react-image-previewer';
 import { CloseButton } from 'react-image-previewer/ui';
-import React from 'react';
 
-const ImageComponent = ({ imgsrc }) => {
-  if (imgsrc == undefined) {
-    return <div>Failed</div>;
+const ImageComponent = ({ imgsrc, alt }: { imgsrc: string; alt?: string }) => {
+  if (!imgsrc) {
+    return (
+      <div>
+        <p>Image not found</p>
+      </div>
+    );
   }
 
   return (
@@ -14,7 +17,7 @@ const ImageComponent = ({ imgsrc }) => {
       }}
     >
       <PhotoView src={imgsrc}>
-        <img src={imgsrc} alt="shit" />
+        <img src={imgsrc} alt={alt ?? ''} />
       </PhotoView>
     </PhotoProvider>
   );
