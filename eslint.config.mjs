8114f5755200b3e39eb72cca136ad3ff0ae38653
Anton Ždanov <anton@azdanov.dev>
2025-07-05 12:18:14 +0300

    try to fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,59 +1,37 @@
-import astroEslintParser from 'astro-eslint-parser';
+import eslint from '@eslint/js';
 import eslintPluginAstro from 'eslint-plugin-astro';
-import globals from 'globals';
-import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import typescriptParser from '@typescript-eslint/parser';
+import globals from 'globals';
 
-export default [
-  js.configs.recommended,
-  ...eslintPluginAstro.configs['flat/recommended'],
-  ...tseslint.configs.recommended,
+export default tseslint.config(
+  {
+    ignores: ['dist', '.astro'],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  eslintPluginAstro.configs.recommended,
   {
     languageOptions: {
       globals: {
         ...globals.browser,
-        ...globals.node,
+      },
+      parserOptions: {
+        // TODO: There is a bug with the parser: https://github.com/ota-meshi/eslint-plugin-astro/issues/447
+        // projectService: {
+        //   allowDefaultProject: ['**/*.astro'],
+        // },
+        // projectService: false,
+        project: './tsconfig.json',
       },
     },
   },
   {
-    files: ['**/*.astro'],
+    files: ['*.astro'],
     languageOptions: {
-      parser: astroEslintParser,
       parserOptions: {
         parser: '@typescript-eslint/parser',
         extraFileExtensions: ['.astro'],
       },
     },
-  },
-  {
-    files: ['**/*.{js,jsx,astro}'],
-    rules: {
-      'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
-    },
-  },
-  {
-    // Define the configuration for `<script>` tag.
-    // Script in `<script>` is assigned a virtual file name with the `.js` extension.
-    files: ['**/*.{ts,tsx}', '**/*.astro/*.js'],
-    languageOptions: {
-      parser: typescriptParser,
-    },
-    rules: {
-      // Note: you must disable the base rule as it can report incorrect errors
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          argsIgnorePattern: '^_',
-          destructuredArrayIgnorePattern: '^_',
-        },
-      ],
-      '@typescript-eslint/no-non-null-assertion': 'off',
-    },
-  },
-  {
-    ignores: ['dist', 'node_modules', '.github', 'types.generated.d.ts', '.astro'],
-  },
-];
+  }
+);
