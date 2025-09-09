d11d3f4fe6ccf694e7d1c3fa64c0170bdbd7fb2b
Anton Ždanov <anton@azdanov.dev>
2025-09-09 19:40:00 +0300

    ci: fix eslint error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
 import eslint from '@eslint/js';
 import eslintPluginAstro from 'eslint-plugin-astro';
 import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 
-export default tseslint.config(
+export default defineConfig(
   {
     ignores: ['dist', '.astro'],
   },
