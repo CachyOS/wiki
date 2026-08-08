5073e88f682e5aef7eb37e52851a855232db66d5
Can Tosun <privat@cantosun.de>
2026-08-08 14:29:34 +0000

    Added translation banner (#602)
    * Added translation banner
    
    * Unjankified the code

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,7 +4,14 @@ import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
 import { z } from 'astro/zod';
 
 export const collections = {
-  docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+  docs: defineCollection({
+    loader: docsLoader(),
+    schema: docsSchema({
+      extend: z.object({
+        ai_translated: z.boolean().optional().default(false),
+      }),
+    }),
+  }),
   i18n: defineCollection({
     loader: i18nLoader(),
     schema: i18nSchema({
